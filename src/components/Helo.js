aaa49f5a1c15c4ab159e165d4a713862754da58c
bleaksmile <toeraab.avf@gmail.com>
2019-03-28 17:50:42 +0100

    changes

--- src/components/Helo.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const Helo = () => <div> cica </div>
-
-export default Helo
--- src/components/MainContent.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const MainContent = ({ children }) => <main>{children}</main>;
-
-export default MainContent;
